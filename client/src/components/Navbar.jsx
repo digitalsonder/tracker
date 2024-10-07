b5b28fbed1f589a2fbc8084ee3a7104701b4d1f6
grandoccasion <abelikovs18@gmail.com>
2024-10-07 16:19:03 +0300

    switched back to on hover events, set them for every link in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,23 +26,40 @@ export default function Navbar() {
     const [logoPopperAnchorEl, setLogoPopperAnchorEl] = useState(null);
     const logoPopperOpen = Boolean(logoPopperAnchorEl);
 
-    const handleLogoPopperToggle = (e) => {
-        if (!logoPopperAnchorEl) {
-            setLogoPopperAnchorEl(e.currentTarget);
-        } else {
-            setLogoPopperAnchorEl(null);
-        };
+    const handleLogoMouseEnter = (e) => {
+        setLogoPopperAnchorEl(e.currentTarget);
     };
 
-    const handleClickAwayLogo = () => {
+    const handleLogoMouseLeave = () => {
         setLogoPopperAnchorEl(null);
+    };
+
+    const [userPopperAnchorEl, setUserPopperAnchorEl] = useState(null);
+    const userPopperOpen = Boolean(userPopperAnchorEl);
+
+    const handleUserMouseEnter = (e) => {
+        setUserPopperAnchorEl(e.currentTarget);
+    };
+
+    const handleUserMouseLeave = () => {
+        setUserPopperAnchorEl(null);
+    };
+
+    const [settingsPopperAnchorEl, setSettingsPopperAnchorEl] = useState(null);
+    const settingsPopperOpen = Boolean(settingsPopperAnchorEl);
+
+    const handleSettingsMouseEnter = (e) => {
+        setSettingsPopperAnchorEl(e.currentTarget);
+    };
+
+    const handleSettingsMouseLeave = () => {
+        setSettingsPopperAnchorEl(null);
     };
 
     return (
         <div className={"navbar " + (scrolled ? "scrolled" : null)} >
-            <ClickAwayListener onClickAway={handleClickAwayLogo}>
-                <div className="logo-holder">
-                    <div className="logo" onClick={handleLogoPopperToggle}>
+                <div className="logo-holder" onMouseLeave={handleLogoMouseLeave}>
+                    <div className="logo" onMouseEnter={handleLogoMouseEnter}>
                         <p className={"logo-text color-themed " + (scrolled ? "scrolled" : null)}>
                             Planabite
                         </p>
@@ -53,17 +70,24 @@ export default function Navbar() {
                     <StyledPopper open={logoPopperOpen} anchorEl={logoPopperAnchorEl}
                     tabs={[{content: '1'}, {content: '2'}]} />
                 </div>
-            </ClickAwayListener>
             <div className="links-holder">
-                <div className={"link account-link " + (scrolled ? "scrolled" : null)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                    </svg>
+                <div className="account-link-holder" onMouseLeave={handleUserMouseLeave}>
+                    <div className={"link account-link " + (scrolled ? "scrolled" : null)} onMouseEnter={handleUserMouseEnter}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+                        <StyledPopper open={userPopperOpen} anchorEl={userPopperAnchorEl}
+                        tabs={[{content: '1'}, {content: '2'}, {content: '3'}]} />
+                    </div>
                 </div>
-                <div className={"link settings-link " + (scrolled ? "scrolled" : null)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-                    </svg>
+                <div className="settings-link-holder" onMouseLeave={handleSettingsMouseLeave}>
+                    <div className={"link settings-link " + (scrolled ? "scrolled" : null)} onMouseEnter={handleSettingsMouseEnter}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                        </svg>
+                        <StyledPopper open={settingsPopperOpen} anchorEl={settingsPopperAnchorEl}
+                        tabs={[{content: '1'}, {content: '2'}, {content: '3'}]} />
+                    </div>
                 </div>
             </div>
         </div>
